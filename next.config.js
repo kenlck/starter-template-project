f154ad04787c06df1a25eefdc238cab5d148c2d0
Ken Low <kenlck1990@gmail.com>
2021-05-24 13:23:00 +0800

    Update config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   // Your build time will be longer with this option
   target: 'serverless',
   future: {
-    webpack5: false,
+    webpack5: true,
     strictPostcssConfiguration: true,
   },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,14 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      screens: {
-        xs: '500px',
-      },
-      colors: {
-        'true-gray': colors.trueGray,
-        'mihas-orange': '#EB5C37',
-        'mihas-blue': '#1C2442',
-      },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
       fontSize: {
         '2xs': '.65rem',
         '3xs': '.55rem',
-      },
-      letterSpacing: {
-        '2widest': '.25em',
-      },
-      height: {
-        'screen-80': '80vh',
-        'screen-60': '60vh',
-      },
-      width: {
-        'screen-60': '60vw',
-      },
-      maxWidth: {
-        56: '14rem',
-      },
-      zIndex: {
-        '-1': '-1',
-      },
-      flex: {
-        2: '2 2 0%',
-        3: '3 3 0%',
       },
     },
   },
